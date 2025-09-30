6c8f127b0300ecc6e3aa7aef6ec1cfb1f7812c37
Denzel Ochieng <ochiengden335@gmail.com>
2025-09-30 22:25:37 +0300

    added complete feature

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let activityInpt = document.getElementById('activity');
 let activityOutput = document.getElementById('activityOutput');
 let addItemBtn = document.getElementById('addItemBtn');
+let bulkRemoveBtn = document.getElementById('rmvBtn');
 
 
 addItemBtn.addEventListener('click', () => {
@@ -10,7 +11,7 @@ addItemBtn.addEventListener('click', () => {
         let activity = document.createElement('li');
         activity.textContent = newActivity;
 
-
+        // Adding a delete button
         let dltBtn = document.createElement('button');
         dltBtn.textContent = 'Delete';
         dltBtn.style.backgroundColor = 'red';
@@ -19,22 +20,50 @@ addItemBtn.addEventListener('click', () => {
         dltBtn.style.marginTop = '5px';
         dltBtn.style.marginBottom = '10px';
 
+        // Adding a complete button
+        let compltBtn = document.createElement('button');
+        compltBtn.textContent = 'Complete';
+        compltBtn.style.backgroundColor = 'green';
+        compltBtn.style.color = 'white';
+        compltBtn.style.marginLeft = '50px';
+        compltBtn.style.marginTop = '5px';
+        compltBtn.style.marginBottom = '10px';
 
+
+        // Addind event listener to the complete button
+        compltBtn.addEventListener('click', () => {
+            activity.style.textDecoration = 'line-through';
+        });
+
+
+        // Adding the event listener to the delete button
         dltBtn.addEventListener('click', () => {
             activity.remove();
         });
 
-
+        // Appending the activity and buttons
         activity.appendChild(dltBtn);
+        activity.appendChild(compltBtn);
         activityOutput.append(activity);
         activityInpt.value = '';
     }
 });
 
 
-activityInpt.addEventListener('keydown', (event) => {
+// Event listener for the enter button
+activityInpt.addEventListener('keypress', (event) => {
     if (event.key === 'Enter') {
         addItemBtn.click();
     }
 });
+
+
+// Creating function for bulk complete
+
+function bulkComplete() {
+    activityOutput.style.textDecoration = 'line-through';
+};
+
+//Event listener for bulk remove button
+bulkRemoveBtn.addEventListener('click', bulkComplete);
 
